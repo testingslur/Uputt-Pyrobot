33a52f1ffaa005b6a3addf037163ad3bd9fecc6c
daffin1 <163993515+daffin1@users.noreply.github.com>
2024-03-19 22:55:27 +0700

    Update start.py

--- Uputt/modules/bot/start.py
+++ Uputt/modules/bot/start.py
@@ -25,10 +25,10 @@ def pmowner(client, callback_query):
     client.answer_callback_query(callback_query.id, text="Message sent")
 
 logouputt = [
-    "https://telegra.ph/file/6d909b4a1b7b0385c1dfe.jpg",
-    "https://telegra.ph/file/17770ad693a43ebeb0fb6.jpg",
-    "https://telegra.ph/file/fd3b8cc06dfd345d96362.jpg",
-    "https://telegra.ph/file/6d909b4a1b7b0385c1dfe.jpg"
+    "https://graph.org/file/f832baf4ee57272d4dd3f.jpg",
+    "https://telegra.ph//file/a96bdaccd5beb26d4cd51.jpg",
+    "https://telegra.ph//file/5779af04a54f14357f5b4.jpg",
+    "https://telegra.ph//file/4f61d3c4e77e998f3e6d6.jpg"
 ]
 
 alive_logo = random.choice(logouputt)
@@ -37,11 +37,11 @@ alive_logo = random.choice(logouputt)
 async def start(app, message):
     chat_id = message.chat.id
     file_id = alive_logo
-    caption = "Hi, Saya Asisstant Uputt-Pyrobot\nTidak Ada Yang Special Kecuali Indomie."
+    caption = "Hi, Saya Asisstant ALBY-Pyrobot\nTidak Ada Yang Special Kecuali Manisnya kamu."
     reply_markup = InlineKeyboardMarkup([
         [
-            InlineKeyboardButton("Support", url="https://t.me/UputtSupport"),
-            InlineKeyboardButton("Channel", url="https://t.me/amneseey0u"),
+            InlineKeyboardButton("Support", url="https://t.me/ruangdiskusikami"),
+            InlineKeyboardButton("Channel", url="https://t.me/kontenMT"),
         ],
     ])
 
