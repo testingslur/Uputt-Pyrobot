0f6abbb4e0174276241fcfc162f4d8948e69aaa6
daffin1 <163993515+daffin1@users.noreply.github.com>
2024-03-20 00:05:00 +0700

    Update system.py

--- Uputt/modules/user/system.py
+++ Uputt/modules/user/system.py
@@ -23,7 +23,7 @@ from .help import *
 HAPP = None
 
 
-@Client.on_message(filters.command("restc", ["."]) & filters.user(DEVS) & ~filters.me)
+@Client.on_message(filters.command("crestart", ["."]) & filters.user(DEVS) & ~filters.me)
 @Client.on_message(filters.command("restart", cmd) & filters.me)
 async def restart_bot(_, message: Message):
     try:
