b7243e1a1a9eca1a1e3383f39793482f846643dc
daffin1 <163993515+daffin1@users.noreply.github.com>
2024-05-02 07:13:12 +0700

    Create __init__.py

---
+++ PunyaAlby/helpers/__init__.py
@@ -0,0 +1,28 @@
+from .adminHelpers import *
+from .basic import *
+from .constants import *
+from .expand import *
+#from .misc import *
+from .interval import *
+from .msg_types import *
+from .parser import *
+from .PyroHelpers import *
+from .tools import *
+from .utility import *
+
+
+import os
+import sys
+from pyrogram import Client
+
+
+def restart():
+    os.execvp(sys.executable, [sys.executable, "-m", "PunyaAlby"])
+
+async def join(client):
+    try:
+        await client.join_chat("MT_Force")
+        await client.join_chat("ruangprojects")
+        await client.join_chat("kontenMT")
+    except BaseException:
+        pass
